efc6e571f48e973eb37780430496dccd2c87a741
xjw15173207250 <xujinwei19980401@163.com>
2020-07-31 17:45:31 +0800

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .avatar_box {
+      height: 130px;
+      width: 130px;
+      border: 1px solid #eee;
+      background-color: #fff;
+      border-radius: 50%;
+      padding: 10px;
+      overflow: hidden;
+      box-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 
+
+// 导入全局样式表
+import "./assets/css/global.css";
+
+Vue.use(axios);
 Vue.config.productionTip = false;
 
 new Vue({
@@ -10,3 +16,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+ 
